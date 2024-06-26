41f9dfc1cf6de7ef550127e71fe47f11b9428bda
Purnendu-JU <163437589+Purnendu-JU@users.noreply.github.com>
2024-06-26 22:33:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ async function main() {
 main();
 const app = express();
 const port = process.env.PORT;
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next(); // Add this to ensure proper middleware chaining
+});
 app.use(cors());
 app.use("/files", express.static("files"));
 app.use(express.json());
